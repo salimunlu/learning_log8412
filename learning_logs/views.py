3b20a7fe5e1c6aefd808e778682623fa522584e5
salim.unlu <salim.unlu@bilgeadam.com>
2024-06-22 13:51:07 +0300

    22 June

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import TopicForm, EntryForm
 from .models import Topic
 
 
@@ -13,3 +14,42 @@ def topics(request):
     topics = Topic.objects.all()
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+
+def topic(request, topic_id):
+    """Show the entries of a specific topic"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':  # Hiçbir veri gönderilmesi; boş bir form oluştur
+        form = TopicForm()
+    else:                         # POST verisi gönderildi, form dolduruldu, bu veriyi işle
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a specific topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
